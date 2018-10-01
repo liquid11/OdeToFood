7f2bfa17ecceb86ba32a8e8a1740a42c0d4b6e86
arsalantezro <arsalan@tezrosolutions.com>
2018-10-01 18:29:01 +0500

    IGreeter

--- IGreeter.cs
+++ IGreeter.cs
@@ -1,11 +1,21 @@
+using Microsoft.Extensions.Configuration;
+
 namespace OdeToFood {
     public interface IGreeter {
         string GetMessageOftheDay ();
     }
 
     public class Greeter : IGreeter {
+
+        private IConfiguration _configuration;
+        public Greeter (IConfiguration configuration) {
+
+            _configuration = configuration;
+
+        }
+
         public string GetMessageOftheDay () {
-            return "Greetings";
+            return _configuration["Greetings"];
         }
     }
 }
